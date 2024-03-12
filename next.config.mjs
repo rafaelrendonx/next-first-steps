f8fdcc1945361b67ed6f6dd77febab3f74e3e570
rafaelrendonx <rafael_x@hotmail.com>
2024-03-11 21:06:27 -0700

    Seccion 2 Terminada

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'standalone',
+  }
+   
+  export default nextConfig
